d46f688f34e766c4ce80a6031f4ff557a9c700bd
spk321ght <spk321.1000@gmail.com>
2025-06-25 04:02:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
